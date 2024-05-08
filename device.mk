ebda254de1ebe37fab55df8b6fdca0826b735500
Edwin Moquete <edwinmmoquete@gmail.com>
2024-05-08 16:59:50 +0700

    NX659J: Build android.media.audio.common.types-V2-cpp
    Needed for WFD, and not built default on QPR2
    
    Change-Id: If860613ef4cb6bae7f7a7e72707c24510e36abd6

--- device.mk
+++ device.mk
@@ -479,6 +479,8 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
+    android.media.audio.common.types-V2-cpp \
+    libavservices_minijail \
     libnl \
     libpng.vendor \
     libwfdaac_vendor
