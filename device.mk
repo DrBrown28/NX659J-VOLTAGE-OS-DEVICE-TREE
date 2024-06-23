db31566528ff0c68f9307b54d38533d9ee49db9d
Bruno Martins <bgcngm@gmail.com>
2024-06-23 21:58:49 +0100

    nx659j: Convert WiFi firmware symlinks to install_symlink targets
    The last bit that was preventing building hermetic images.
    
    Change-Id: I75bb7de4593a7992f6015317cf97e088beeba0cb

--- device.mk
+++ device.mk
@@ -468,3 +468,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libpng.vendor:32 \
     libwfdaac_vendor
+ 
+# WiFi firmware symlinks
+ PRODUCT_PACKAGES += \
+     firmware_wlan_mac.bin_symlink \
+     firmware_WCNSS_qcom_cfg.ini_symlink    
