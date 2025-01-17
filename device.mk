54623b5c41fe64688d6b6497b5b4eaf587f0ee7a
LuK1337 <priv.luk@gmail.com>
2025-01-17 17:06:51 +0100

    nx659j: Switch to common lights HAL
    Change-Id: If515e7c3f6e5b6895076f1d8381fb2c22bd1b3de

--- device.mk
+++ device.mk
@@ -198,7 +198,7 @@ PRODUCT_PACKAGES += \
 
 # Light
 PRODUCT_PACKAGES += \
-    android.hardware.lights-service.nx659j
+    android.hardware.lights-service.lineage
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
