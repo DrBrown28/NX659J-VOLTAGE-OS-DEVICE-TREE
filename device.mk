59759dd3da72aca1caeb2c616eb2fbd4f48a6756
DrBrown28 <112201610+DrBrown28@users.noreply.github.com>
2025-01-20 15:33:42 -0400

    NX659j: Delete some unused camera libs

--- device.mk
+++ device.mk
@@ -85,22 +85,17 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.camera.device@3.5:64 \
-    android.hardware.camera.provider@2.4-impl:32 \
-    android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.6:64
-   
-PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
     libcamera2ndk_vendor \
     libgui_vendor \
     liblz4.vendor \
     libstdc++_vendor \
-    libutilscallstack.vendor \
-    vendor.qti.hardware.camera.device@1.0.vendor \
-    vendor.qti.hardware.camera.postproc@1.0.vendor
-
+    libutilscallstack.vendor
+    
 # config.fs
 PRODUCT_PACKAGES += \
     fs_config_files
