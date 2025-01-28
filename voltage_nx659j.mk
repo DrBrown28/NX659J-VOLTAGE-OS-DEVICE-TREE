3130dbf603aed452ddba594d5dc9abe4eff52381
DrBrown28 <112201610+DrBrown28@users.noreply.github.com>
2025-01-28 17:27:19 -0400

    NX659J: Disable OMX service Change-Id: I5381e098af64271f9d71c484262e1710e8fcacea

--- voltage_nx659j.mk
+++ voltage_nx659j.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020-2025 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
 
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/voltage/config/common_full_phone.mk)
