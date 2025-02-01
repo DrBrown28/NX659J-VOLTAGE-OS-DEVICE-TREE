963723d6e7b3d7f1f87e9fef2b9c485b1a906e41
DrBrown28 <112201610+DrBrown28@users.noreply.github.com>
2025-02-01 17:13:25 -0400

    NX659J: Build android.hardware.drm@1.4 on vendor
    The proprietary widevine service expects it to be available
    in the vendor partition.
    Hence, build vendor variant of android.hardware.drm@1.4.
    
    Change-Id: I03cc549de04043395ad3197a6770a2e4450506f6 | AOSP

--- device.mk
+++ device.mk
@@ -128,10 +128,12 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4.vendor \
-    android.hardware.drm-service.clearkey \
+    android.hardware.drm@1.4-service.clearkey \
     libhidlmemory.vendor
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
 
 # fastbootd
 PRODUCT_PACKAGES += \
