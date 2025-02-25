05a836206410555ae6079d07008def3f4993eac0
DrBrown28 <112201610+DrBrown28@users.noreply.github.com>
2025-02-25 08:26:30 -0400

    nx659j: remove vendor/lineage/config/device_framework_matrix.xml Not needed in voltageos android 15 build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,8 +118,7 @@ TARGET_BOARD_PLATFORM := kona
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    $(DEVICE_PATH)/framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 
