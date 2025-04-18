f37ef71d5f0b08e2f3921631e068869252ba7d95
DrBrown28 <112201610+DrBrown28@users.noreply.github.com>
2025-04-18 10:46:39 -0400

    Revert "NX659J: Enable elf checks"
    This reverts commit ce4fefe53aa2417c9a281c683f2b42d346a85665.

--- extract-files.py
+++ extract-files.py
@@ -26,8 +26,6 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 LINEAGE_ROOT="${MY_DIR}"/../../..
 
-export TARGET_ENABLE_CHECKELF=true
-
 HELPER="${LINEAGE_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
